5b763c124675fcc105cb318353050b1a81abb3a2
Remir <tengubird800@gmail.com>
2022-09-15 21:10:35 +0300

    third commit

--- first/Program.cs
+++ first/Program.cs
@@ -1,12 +1,13 @@
 ﻿// int num = 12;
 // Console.WriteLine("Месяцев в году " + num + num);
 
-Console.WriteLine("How old are you?");
-int age = int.Parse(Console.ReadLine());
-Console.WriteLine("You are " + age + " years old");
+// Console.WriteLine("How old are you?");
+// int age = int.Parse(Console.ReadLine());
+// Console.WriteLine("You are " + age + " years old");
 
-Console.WriteLine("Введите число a");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число b");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine(a + b);
+// Console.WriteLine("Введите число a");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число b");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine(a + b);
+
