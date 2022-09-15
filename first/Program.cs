4e4107c5900369bb11e0a51de4bc88afea8fc835
Remir <tengubird800@gmail.com>
2022-09-15 21:41:11 +0300

    fifth commit

--- first/Program.cs
+++ first/Program.cs
@@ -12,21 +12,36 @@
 // Console.WriteLine(a + b);
 
 Console.WriteLine("Вы попали в игру угадай число");
-int secret_number = 4;
-while (true)
+int secret_number = new Random().Next(1, 11);
+int attempt = 5;
+bool win = false;
+while (attempt > 0)
 {
-    Console.WriteLine("Введите число от 1 до 10: ");
+    Console.Write("Введите число от 1 до 10: ");
     int number = int.Parse(Console.ReadLine());
-    if(number > secret_number)
+    if (number > secret_number)
     {
         Console.WriteLine("Секретное число меньше");
+        attempt--;
     }
-    else if(number < secret_number)
+    else if (number < secret_number)
     {
         Console.WriteLine("Секретное число больше");
+        attempt--;
     }
-    else if(number == secret_number)
+    else if (number == secret_number)
     {
         Console.WriteLine("Вы угадали!!");
+        win = true;
+        break;
     }
+    Console.WriteLine("  Осталось попыток " + attempt);
+}
+if(win)
+{
+    Console.WriteLine("  Вы победили, увидимся в следующий раз!");
+}else
+{
+    System.Console.WriteLine("  Вы проиграли, секретное число - " + secret_number);
+    System.Console.WriteLine("  Попробуйте еще раз :)");
 }
