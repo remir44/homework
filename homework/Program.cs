56dacf8c36d59396d791053b3e919ba3e4ac19d6
Remir <tengubird800@gmail.com>
2022-09-19 14:19:03 +0300

    First task done

---
+++ homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// Первая задача
+Console.WriteLine("Введите число a:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b:");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine("Число a = " + a + " максимальное.");
+}
+else
+{
+    Console.WriteLine("Число b = " + b + " максимальное.");
+}
