e94a7d63ff452bee05bfca352cec670bf455d79f
Remir <tengubird800@gmail.com>
2022-09-19 14:40:32 +0300

    Third task done

--- homework/Program.cs
+++ homework/Program.cs
@@ -13,19 +13,32 @@
 // }
 
 // Задача 4:
-Console.Write("Введите число a: ");
-int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число b: ");
-int b = int.Parse(Console.ReadLine()!);
-Console.Write("Введите число c: ");
-int c = int.Parse(Console.ReadLine()!);
-int max = a;
-if (b > max)
+// Console.Write("Введите число a: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число b: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число c: ");
+// int c = int.Parse(Console.ReadLine()!);
+// int max = a;
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// Console.WriteLine("Максимальное число = " + max);
+
+// Задача 6:
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 2 != 0)
 {
-    max = b;
+    Console.WriteLine("Число " + num + " не четное.");
 }
-if (c > max)
+else
 {
-    max = c;
+    Console.WriteLine("Число " + num + " четное.");
 }
-Console.WriteLine("Максимальное число = " + max);
